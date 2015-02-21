ae60b96cb496a709253d80aacf6da70bbfe9b65e
Osamu Kobayashi <osamu-k@arion.ocn.ne.jp>
2015-02-21 22:59:12 +0900

    A test is added; AddTwoFractionsCommonDenominator

--- fraction001/main.cpp
+++ fraction001/main.cpp
@@ -1,5 +1,44 @@
 #include "gmock/gmock.h"
 
+using namespace testing;
+
+extern "C" {
+
+void listAdd(
+    int sign1, unsigned int numerator1, unsigned int denominator1,
+    int sign2, unsigned int numerator2, unsigned int denominator2,
+    int *signA, unsigned int *numeratorA, unsigned int *denominatorA )
+{
+    *signA = 1;
+    *numeratorA = 5u;
+    *denominatorA = 7u;
+}
+
+}
+
+TEST(Fraction, AddTwoFractionsCommonDenominator)
+{
+    int sign1 = 1;
+    unsigned int numerator1 = 2;
+    unsigned int denominator1 = 7;
+
+    int sign2 = 1;
+    unsigned int numerator2 = 3;
+    unsigned int denominator2 = 7;
+
+    int signA = 0;
+    unsigned int numeratorA = 0;
+    unsigned int denominatorA = 0;
+
+    listAdd( sign1, numerator1, denominator1,
+             sign2, numerator2, denominator2,
+             &signA, &numeratorA, &denominatorA );
+
+    ASSERT_THAT( signA, Eq(1) );
+    ASSERT_THAT( numeratorA, Eq(5u) );
+    ASSERT_THAT( denominatorA, Eq(7u) );
+}
+
 int main(int argc, char **argv)
 {
     ::testing::InitGoogleMock(&argc,argv);
