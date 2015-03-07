811084f5f88b24b93915d470405efde3c01e5207
Osamu Kobayashi <osamu-k@arion.ocn.ne.jp>
2015-03-07 09:33:31 +0900

    New test AddFractionsOfCommonDenominator2

--- fraction003/main.cpp
+++ fraction003/main.cpp
@@ -12,7 +12,10 @@ typedef struct {
 
 fraction fractionAdd( fraction f1, fraction f2 )
 {
-    fraction result = { +1, 2u+3u, 7u };
+    fraction result;
+    result.sign = +1;
+    result.numerator = f1.numerator + f2.numerator;
+    result.denominator = f1.denominator;
     return result;
 }
 
@@ -28,6 +31,18 @@ TEST( Fraction, AddFractionsOfCommonDenominator )
     ASSERT_THAT( f3.sign, Eq(+1) );
     ASSERT_THAT( f3.numerator, Eq(2u+3u) );
     ASSERT_THAT( f3.denominator, Eq(7u) );
+}
+
+TEST( Fraction, AddFractionsOfCommonDenominator2 )
+{
+    fraction f1 = { +1, 1u, 5u };
+    fraction f2 = { +1, 2u, 5u };
+    fraction f3 = { 0, 0, 0 };
+
+    f3 = fractionAdd( f1, f2 );
+    ASSERT_THAT( f3.sign, Eq(+1) );
+    ASSERT_THAT( f3.numerator, Eq(1u+2u) );
+    ASSERT_THAT( f3.denominator, Eq(5u) );
 }
 
 int main(int argc, char **argv)
