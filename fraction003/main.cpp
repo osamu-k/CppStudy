329ccbf40c5557f1e496f932fc571b876dfe5774
Osamu Kobayashi <osamu-k@arion.ocn.ne.jp>
2015-03-07 09:38:21 +0900

    New test AddFractionsOfDifferentDenominator

--- fraction003/main.cpp
+++ fraction003/main.cpp
@@ -14,8 +14,14 @@ fraction fractionAdd( fraction f1, fraction f2 )
 {
     fraction result;
     result.sign = +1;
-    result.numerator = f1.numerator + f2.numerator;
-    result.denominator = f1.denominator;
+    if( f1.denominator == f2.denominator ){
+        result.numerator = f1.numerator + f2.numerator;
+        result.denominator = f1.denominator;
+    }
+    else{
+        result.numerator = f1.numerator * f2.denominator + f2.numerator * f1.denominator;
+        result.denominator = f1.denominator * f2.denominator;
+    }
     return result;
 }
 
@@ -43,6 +49,18 @@ TEST( Fraction, AddFractionsOfCommonDenominator2 )
     ASSERT_THAT( f3.sign, Eq(+1) );
     ASSERT_THAT( f3.numerator, Eq(1u+2u) );
     ASSERT_THAT( f3.denominator, Eq(5u) );
+}
+
+TEST( Fraction, AddFractionsOfDifferentDenominator )
+{
+    fraction f1 = { +1, 2u, 5u };
+    fraction f2 = { +1, 3u, 7u };
+    fraction f3 = { 0, 0, 0 };
+
+    f3 = fractionAdd( f1, f2 );
+    ASSERT_THAT( f3.sign, Eq(+1) );
+    ASSERT_THAT( f3.numerator, Eq(2u * 7u + 3u * 5u) );
+    ASSERT_THAT( f3.denominator, Eq(5u * 7u) );
 }
 
 int main(int argc, char **argv)
